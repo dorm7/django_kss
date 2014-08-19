bf2d3eab75291e55a6bde600d8b875dce1ef5f8a
tim hsu <tim.yellow@gmail.com>
2014-08-19 16:05:59 +0800

    update to remove the stupid setting

--- django_kss_project/manage.py
+++ django_kss_project/manage.py
@@ -2,9 +2,13 @@
 import os
 import sys
 
+dirname = os.path.dirname(os.path.dirname( os.path.abspath( __file__)))
+sys.path.append(
+    dirname
+)
+print "==="
+print dirname
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_kss_project.settings")
-
     from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)
