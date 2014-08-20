e133d99d470ac7ae1b65574f2e4b26826662d245
tim hsu <tim.yellow@gmail.com>
2014-08-20 22:20:53 +0800

    update with no extra js

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-kss',
-    version='0.1',
+    version='0.1.1',
     packages=['django_kss'],
     include_package_data=True,
     license='BSD License',  # example license
