412bb0910300e25488693a4c38e2428ecdd6f03d
tim <tim@timdeMacBook-Pro.local>
2014-11-21 20:34:28 +0800

    update setup file

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-kss',
-    version='0.1.3',
+    version='0.1.4',
     packages=['django_kss'],
     include_package_data=True,
     license='BSD License',  # example license
