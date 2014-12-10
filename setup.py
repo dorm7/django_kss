8b3f7b06849536f27fa86435686d9d685aae9845
tim <tim@timdeMacBook-Pro.local>
2014-12-10 17:59:35 +0800

    fix bug while li has extray margin

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-kss',
-    version='0.1.4',
-    packages=['django_kss'],
+    version='0.1.7',
+    packages=['django_kss', 'django_kss.templatetags'],
     include_package_data=True,
     license='BSD License',  # example license
     description='A simple Django app to make styleguide',
