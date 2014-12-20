a37588f18d3c6cc8e305ccc45865298fe7982598
tim <tim@timdeMacBook-Pro.local>
2014-12-20 20:10:21 +0800

    update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-kss',
-    version='0.1.8',
+    version='0.1.9',
     packages=['django_kss', 'django_kss.templatetags'],
     include_package_data=True,
     license='BSD License',  # example license
