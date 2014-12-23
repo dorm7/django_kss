00a82cbe3f058f31643b5cf85db3c03cf40fd6c2
tim <tim@timdeMacBook-Pro.local>
2014-12-23 11:34:35 +0800

    update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-kss',
-    version='0.1.12',
+    version='0.1.13',
     packages=find_packages() ,
     include_package_data=True,
     license='BSD License',  # example license
