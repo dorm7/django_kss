fc7cc2b59ff23c9418bf02eff87ae167ed079c77
tim <tim@timdeMacBook-Pro.local>
2015-02-22 18:54:42 +0800

    udpate

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-kss-styleguide',
-    version='0.5.8',
+    version='0.5.9',
     packages=find_packages() ,
     include_package_data=True,
     license='BSD License',  # example license
